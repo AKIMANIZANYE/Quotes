bcd3b6469ae9e1df5b5d7aaf9139d11659db8b5f
AKIMANIZANYE <akimanizanye.claudine@gmail.com>
2019-01-31 13:27:57 +0300

    added  new quote

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     public showDescription:boolean
-    constructor(public Title:string, public quote:string, public Person:string,public completeDate:Date){
+    constructor(public Title:string,public Author:string, public quote:string,public user:string,public completeDate:Date ,public upvote:number,public downvote:number){
         this.showDescription=false
 
     }

85db1bb28e2ab172b323c87974f1f4778f7a259d
AKIMANIZANYE <akimanizanye.claudine@gmail.com>
2019-01-31 08:50:04 +0300

    I added the delete button

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,9 @@
-export class quote {
-    constructor(public id:number, public name:string,public description:string){}
-}
+export class Quote {
+    public showDescription:boolean;
+    constructor(public id:number, public name:string,public description:string){
+        this.showDescription=false
 
+    }
+}
 
 

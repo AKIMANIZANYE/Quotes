fd2168aa8a8ea33a0590d74cfe4171f45d02ff06
AKIMANIZANYE <akimanizanye.claudine@gmail.com>
2019-01-31 09:43:05 +0300

    I added the  styles

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,12 +8,12 @@ import {Quote, } from '../quote'
 export class QuoteComponent implements OnInit {
 
     Quotes = [
-      new Quote(1,'Watch Finding Nemo','Find an online version and watch merlin find his son'),
-        new Quote(2,'Buy Cookies','I have to buy cookies for the parrot'),
-        new Quote(3, 'Get new Phone Case','Diana has her birthday coming up soon'),
-        new Quote(4, 'Get Dog Food','Pupper likes expensive sancks'),
-        new Quote(5, 'Solve math homework','Damn Math'),
-        new Quote(6, 'Plot my world domination plan','Cause I am an evil overlord'),
+      new Quote(1,'Watch Finding Nemo','Find an online version and watch merlin find his son',new Date(2018,3,14)),
+        new Quote(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2018,3,14)),
+        new Quote(3, 'Get new Phone Case','Diana has her birthday coming up soon',new Date(2018,3,14)),
+        new Quote(4, 'Get Dog Food','Pupper likes expensive sancks',new Date(2018,3,14)),
+        new Quote(5, 'Solve math homework','Damn Math',new Date(2018,3,14)),
+        new Quote(6, 'Plot my world domination plan','Cause I am an evil overlord',new Date(2018,3,14)),
 
     ]
     toogleDetails(index){

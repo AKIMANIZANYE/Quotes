1641a50a2da49d44999038be44aee003d80a3673
AKIMANIZANYE <akimanizanye.claudine@gmail.com>
2019-01-31 13:29:17 +0300

    added  upvote and downvote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,12 +9,12 @@ import {Quote} from '../quote'
 
  export class QuoteComponent implements OnInit {
   Quote = [
-      new quote('Quote about Stress','Anne','Do not stress yourself', 'Claudine',new Date(2019,1,12),0,0),
+      new Quote('Quote about Stress','Anne','Do not stress yourself', 'Claudine',new Date(2019,1,12),0,0),
       new Quote('Motivation Quote', 'Alexia','Never giveup', 'Ciara',new Date(1993,8,4),0,0 ),
       new Quote ( 'Courageous Quotes', 'Anneheyman','Try to fail but never fail to try', 'Claire', new Date(1998,9,4),0,0 ),
-      new Quote( 'Women in Tech', 'cosntantine','“Life is a series of building, testing, changing and iterating.”', 'Maliza',new Date(1918,8,24) ),
+      new Quote( 'Women in Tech', 'cosntantine','“Life is a series of building, testing, changing and iterating.”', 'Maliza',new Date(1918,8,24),0,0),
       new Quote('Underreted Women', 'Alfred','“Trust in yourself, believe that your voice matters, and know that your words are good enough.”','Clemantine' ,new Date(1956,6,14),0,0 ),
-      new Quotes( 'Quote about believe','lilianne','“Love what you do and do what you love. " ', 'Clarisse', new Date(1997,9,14) ,0,0),
+      new Quote( 'Quote about believe','lilianne','“Love what you do and do what you love. " ', 'Clarisse', new Date(1997,9,14) ,0,0),
 
   ]
   addNewGoal(goal){

6b81b0d1dfb4c4ef700092dabb37eb0f8aefad2b
AKIMANIZANYE <akimanizanye.claudine@gmail.com>
2019-02-01 16:20:03 +0300

    i removed /dist

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     public showDescription:boolean;
-    constructor(public Title:string,public Author:string, public quote:string,public user:string,public completeDate:Date ,public upvote:number,public downvote:number){
+    constructor(public title:string,public author:string, public quote:string,public user:string,public completeDate:Date ,public upvote:number,public downvote:number){
         this.showDescription=false;
 
     }

00e4ea44472b52bf458824f79f3c2ebc3b07f413
AKIMANIZANYE <akimanizanye.claudine@gmail.com>
2019-02-01 09:07:54 +0300

    i added quote in form

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,7 +1,7 @@
 export class Quote {
-    public showDescription:boolean
+    public showDescription:boolean;
     constructor(public Title:string,public Author:string, public quote:string,public user:string,public completeDate:Date ,public upvote:number,public downvote:number){
-        this.showDescription=false
+        this.showDescription=false;
 
     }
 }
